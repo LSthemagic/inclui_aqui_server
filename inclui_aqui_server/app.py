883034ef4bbe2d936dfca0562d0d1b81555634c9
Railan Santana <lansilvah14fsa@gmail.com>
2025-06-22 23:12:08 -0300

    fix: corrigir o modelo de resposta do endpoint de verificação de saúde

--- inclui_aqui_server/app.py
+++ inclui_aqui_server/app.py
@@ -57,7 +57,7 @@ app = FastAPI(
 app.include_router(api_router_v1)
 
 
-@app.get('/', response_model=response_schema)
+@app.get('/', response_model=GenericResponseModel)
 def health_check():
     return GenericResponseModel(
         message='Root module is running',
